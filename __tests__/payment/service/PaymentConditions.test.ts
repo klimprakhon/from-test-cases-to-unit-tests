3ef50a1f8757553e246d695b8a9c240838e6240f
klimprakhon <klimprakhon@gmail.com>
2025-10-24 11:48:05 +0700

    refactor(test-payment): separate one test file per one class

--- __tests__/payment/service/PaymentConditions.test.ts
+++ __tests__/payment/service/PaymentConditions.test.ts
@@ -1,7 +1,4 @@
-import {
-  PaymentConditions,
-  PaymentCalculator,
-} from "../../src/payment/payment.service";
+import { PaymentConditions } from "../../../src/payment/payment.service";
 
 describe("Payment Service - Check Date Type", () => {
   it("It should return false If Session Date is 13th Oct and the 13th is Day Off", () => {
@@ -19,7 +16,7 @@ describe("Payment Service - Check Date Type", () => {
     );
 
     // Assert
-    expect(expected).toEqual(actual);
+    expect(actual).toEqual(expected);
   });
 
   it("It should return true If Session Date is 16th Oct and the 13th is Day Off", () => {
@@ -37,7 +34,7 @@ describe("Payment Service - Check Date Type", () => {
     );
 
     // Assert
-    expect(expected).toEqual(actual);
+    expect(actual).toEqual(expected);
   });
 });
 
@@ -65,7 +62,7 @@ describe("Payment Service - Check Time Period", () => {
     const actual = paymentService.isOnPeak(onPeakPeriod, sessionPeriod);
 
     // Assert
-    expect(expected).toEqual(actual);
+    expect(actual).toEqual(expected);
   });
 
   it("TC2: It should return false If Session started at 08:30 and ended at 09:00", () => {
@@ -96,7 +93,7 @@ describe("Payment Service - Check Time Period", () => {
     const actual = paymentService.isOnPeak(onPeakPeriod, sessionPeriod);
 
     // Assert
-    expect(expected).toEqual(actual);
+    expect(actual).toEqual(expected);
   });
 
   it("TC3: It should return true If Session started at 08:00 and ended at 10:30", () => {
@@ -127,7 +124,7 @@ describe("Payment Service - Check Time Period", () => {
     const actual = paymentService.isOnPeak(onPeakPeriod, sessionPeriod);
 
     // Assert
-    expect(expected).toEqual(actual);
+    expect(actual).toEqual(expected);
   });
 
   it("TC4: It should return true If Session started at 06:00 and ended at 20:00", () => {
@@ -158,7 +155,7 @@ describe("Payment Service - Check Time Period", () => {
     const actual = paymentService.isOnPeak(onPeakPeriod, sessionPeriod);
 
     // Assert
-    expect(expected).toEqual(actual);
+    expect(actual).toEqual(expected);
   });
 
   it("TC5: It should return true If Session started at 06:30 and ended at 22:00", () => {
@@ -194,7 +191,7 @@ describe("Payment Service - Check Time Period", () => {
     const actual = paymentService.isOnPeak(onPeakPeriod, sessionPeriod);
 
     // Assert
-    expect(expected).toEqual(actual);
+    expect(actual).toEqual(expected);
   });
 
   it("TC6: It should return true If Session started at 07:45 and ended at 23:30", () => {
@@ -230,7 +227,7 @@ describe("Payment Service - Check Time Period", () => {
     const actual = paymentService.isOnPeak(onPeakPeriod, sessionPeriod);
 
     // Assert
-    expect(expected).toEqual(actual);
+    expect(actual).toEqual(expected);
   });
 
   it("TC8: It should return false If Session started at 09:00 and ended at 09:00", () => {
@@ -256,7 +253,7 @@ describe("Payment Service - Check Time Period", () => {
     const actual = paymentService.isOnPeak(onPeakPeriod, sessionPeriod);
 
     // Assert
-    expect(expected).toEqual(actual);
+    expect(actual).toEqual(expected);
   });
 
   it("TC9: It should return false If Session started at 09:00 and ended at 12:00", () => {
@@ -287,9 +284,7 @@ describe("Payment Service - Check Time Period", () => {
     const actual = paymentService.isOnPeak(onPeakPeriod, sessionPeriod);
 
     // Assert
-    console.log("expected", expected);
-    console.log("actual", actual);
-    expect(expected).toEqual(actual);
+    expect(actual).toEqual(expected);
   });
 
   it("TC10: It should return false If Session started at 09:00 and ended at 13:00", () => {
@@ -320,7 +315,7 @@ describe("Payment Service - Check Time Period", () => {
     const actual = paymentService.isOnPeak(onPeakPeriod, sessionPeriod);
 
     // Assert
-    expect(expected).toEqual(actual);
+    expect(actual).toEqual(expected);
   });
 
   it("TC11: It should return false If Session started at 09:00 and ended at 22:00", () => {
@@ -356,165 +351,6 @@ describe("Payment Service - Check Time Period", () => {
     const actual = paymentService.isOnPeak(onPeakPeriod, sessionPeriod);
 
     // Assert
-    expect(expected).toEqual(actual);
-  });
-});
-
-describe("Payment Service - AP amount per Session", () => {
-  it("It should return 0.00 if Joined time is 59 mins", () => {
-    // Arrange
-    const expectedAPAmount = 0.0;
-    const paymentService = new PaymentCalculator();
-    // Act
-    const actualAPAmount = paymentService.calculateAvailabilityPayment(59);
-
-    // Assert
-    expect(actualAPAmount).toEqual(expectedAPAmount);
-  });
-
-  it("It should return 4.00 if Joined time is 60 mins", () => {
-    // Arrange
-    const expectedAPAmount = 4.0;
-    const paymentService = new PaymentCalculator();
-
-    // Act
-    const actualAPAmount = paymentService.calculateAvailabilityPayment(60);
-
-    // Assert
-    expect(actualAPAmount).toEqual(expectedAPAmount);
-  });
-
-  it("It should return 4.00 if Joined time is 101 mins", () => {
-    // Arrange
-    const expectedAPAmount = 4.0;
-    const paymentService = new PaymentCalculator();
-
-    // Act
-    const actualAPAmount = paymentService.calculateAvailabilityPayment(101);
-
-    // Assert
-    expect(actualAPAmount).toEqual(expectedAPAmount);
-  });
-
-  it("It should return 8.00 if Joined time is 120 mins", () => {
-    // Arrange
-    const expectedAPAmount = 8.0;
-    const paymentService = new PaymentCalculator();
-
-    // Act
-    const actualAPAmount = paymentService.calculateAvailabilityPayment(120);
-
-    // Assert
-    expect(actualAPAmount).toEqual(expectedAPAmount);
-  });
-
-  it("It should return 8.00 if Joined time is 121 mins", () => {
-    // Arrange
-    const expectedAPAmount = 8.0;
-    const paymentService = new PaymentCalculator();
-
-    // Act
-    const actualAPAmount = paymentService.calculateAvailabilityPayment(121);
-
-    // Assert
-    expect(actualAPAmount).toEqual(expectedAPAmount);
-  });
-
-  it("It should return 24.00 if Joined time is 180 mins", () => {
-    // Arrange
-    const expectedAPAmount = 24.0;
-    const paymentService = new PaymentCalculator();
-
-    // Act
-    const actualAPAmount = paymentService.calculateAvailabilityPayment(180);
-
-    // Assert
-    expect(actualAPAmount).toEqual(expectedAPAmount);
-  });
-
-  it("It should return 24.00 if Joined time is 181 mins", () => {
-    // Arrange
-    const expectedAPAmount = 24.0;
-    const paymentService = new PaymentCalculator();
-
-    // Act
-    const actualAPAmount = paymentService.calculateAvailabilityPayment(181);
-
-    // Assert
-    expect(actualAPAmount).toEqual(expectedAPAmount);
-  });
-
-  it("It should return 40.00 if Joined time is 240 mins", () => {
-    // Arrange
-    const expectedAPAmount = 40.0;
-    const paymentService = new PaymentCalculator();
-
-    // Act
-    const actualAPAmount = paymentService.calculateAvailabilityPayment(240);
-
-    // Assert
-    expect(actualAPAmount).toEqual(expectedAPAmount);
-  });
-
-  it("It should return 56.00 if Joined time is 300 mins", () => {
-    // Arrange
-    const expectedAPAmount = 56.0;
-    const paymentService = new PaymentCalculator();
-
-    // Act
-    const actualAPAmount = paymentService.calculateAvailabilityPayment(300);
-
-    // Assert
-    expect(actualAPAmount).toEqual(expectedAPAmount);
-  });
-
-  it("It should return 184.00 if Joined time is 780 mins", () => {
-    // Arrange
-    const expectedAPAmount = 184.0;
-    const paymentService = new PaymentCalculator();
-
-    // Act
-    const actualAPAmount = paymentService.calculateAvailabilityPayment(780);
-
-    // Assert
-    expect(actualAPAmount).toEqual(expectedAPAmount);
-  });
-});
-
-describe("Payment Service - EP amount per Session", () => {
-  it("It should return 2.50 if purchased energy is 0.5 kWh", () => {
-    // Arrange
-    const expectedEPAmount = 2.5;
-    const paymentService = new PaymentCalculator();
-
-    // Act
-    const actualEPAmount = paymentService.calculateEnergyPayment(0.5);
-
-    // Assert
-    expect(actualEPAmount).toEqual(expectedEPAmount);
-  });
-
-  it("It should return 5.0 if purchased energy is 1 kWh", () => {
-    // Arrange
-    const expectedEPAmount = 5.0;
-    const paymentService = new PaymentCalculator();
-
-    // Act
-    const actualEPAmount = paymentService.calculateEnergyPayment(1);
-
-    // Assert
-    expect(actualEPAmount).toEqual(expectedEPAmount);
-  });
-
-  it("It should return 7.5 if purchased energy is 1.5 kWh", () => {
-    // Arrange
-    const expectedEPAmount = 7.5;
-    const paymentService = new PaymentCalculator();
-
-    // Act
-    const actualEPAmount = paymentService.calculateEnergyPayment(1.5);
-
-    // Assert
-    expect(actualEPAmount).toEqual(expectedEPAmount);
+    expect(actual).toEqual(expected);
   });
 });
